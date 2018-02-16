fb193e32a8230c5b5b0a7bfb88653c342db15234
Confettimaker <jeajohnadams@gmail.com>
2018-02-16 00:28:01 -0600

    Updated files to follow Python spacing standards

--- sqlitewrapper.py
+++ sqlitewrapper.py
@@ -1,20 +1,20 @@
 import sqlite3 as sql3
 
 class Database(object):
-  def __init__(self, database):
-    self.connection = sql3.connect(database)
-    self.cursor = self.connection.cursor()
-    self.result = self.cursor.fetchall()
-  
-  def close(self):
-    self.connection.close()
+    def __init__(self, database):
+        self.connection = sql3.connect(database)
+        self.cursor = self.connection.cursor()
+        self.result = self.cursor.fetchall()
 
-  def save(self):
-    self.connection.commit()
-  
-  def command(self, command):
-    self.cursor.execute(command)
-    self.result = self.cursor.fetchall()
+    def close(self):
+        self.connection.close()
 
-  def fetch(self):
-    return self.result
+    def save(self):
+        self.connection.commit()
+
+    def command(self, command):
+        self.cursor.execute(command)
+        self.result = self.cursor.fetchall()
+
+    def fetch(self):
+        return self.result
